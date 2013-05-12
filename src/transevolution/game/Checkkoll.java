328250a8e9c232faeb0978ed7a506b1699fba4d2
Milos Ziolkowski <milos.ziolkowski@uni-duesseldorf.de>
2013-05-12 23:22:15 +0200

    Jack bewegt sich, Checkkollklasse hinzugefügt

---
+++ src/transevolution/game/Checkkoll.java
@@ -0,0 +1,81 @@
+package transevolution.game;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Polygon;
+
+//Hier werden Kollisionen überprüft, indem alle Objekte in Arrays gespeichert werden
+
+public abstract class Checkkoll 
+{
+  
+  private static int sizeX = 31;
+  private static int sizeY = 31;
+  protected int x;
+  protected int y;
+  protected Polygon kollisionsFlaeche;
+
+
+  public abstract void draw(Graphics g) throws SlickException;
+ 
+  
+  
+  public void update(int delta) throws SlickException {
+  };
+  
+
+  //x und y-koordinate der Spielobjekte
+  public Checkkoll(int x, int y) throws SlickException 
+  {
+    this.x = x;
+    this.y = y;
+    kollisionsFlaeche = new Polygon(new float[] { x, y, x + sizeX, y, x + sizeX, y + sizeY, x, y + sizeY });
+  }
+//X und Y Position
+  public int getX() 
+  {
+    return x;
+  }
+  
+  public int getY() 
+  {
+    return y;
+  }
+
+  public Polygon getkollFlaeche() 
+  {
+    return kollisionsFlaeche;
+  }
+
+  
+  //tatsächliche Kollisionsüberprüfung
+  public boolean pruefeKollsion(Checkkoll spielObjekt) 
+  {
+    return kollisionsFlaeche.intersects(spielObjekt.getkollFlaeche());
+  }
+  
+  //neue Arraylisten
+  public ArrayList<Checkkoll> pruefeKollsion(ArrayList<Checkkoll> spObj) 
+  {
+	  ArrayList<Checkkoll> objKoll = new ArrayList<Checkkoll>();
+	  
+	  for (Checkkoll obj : spObj) 
+	  {
+		if (obj != null) 
+		{
+			if (obj.pruefeKollsion(this)) 
+			{
+				objKoll.add(obj);
+			}
+		}
+	  }
+    return objKoll;
+  }
+  
+  //Anzahl der Kollisionen
+  public boolean pruefePolyKollision(Polygon flaeche) 
+  {
+    return kollisionsFlaeche.intersects(flaeche);
+  }
+}
--- src/transevolution/game/StartGame.java
+++ src/transevolution/game/StartGame.java
@@ -2,6 +2,7 @@ package transevolution.game;
 
 import java.awt.Font;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
@@ -18,6 +19,8 @@ import org.newdawn.slick.tiled.TiledMap;
 import org.newdawn.slick.util.ResourceLoader;
 
 
+
+
 public class StartGame extends BasicGameState
 {
 	// stateID für das StartGame gleich 1
@@ -29,8 +32,11 @@ public class StartGame extends BasicGameState
 	
 	//map
 	protected TiledMap map;
-	// SpielObjekt: Spieler
-	
+	// Kollisionsobjekte
+		//Jack
+	protected ArrayList<Checkkoll> jackobj = new ArrayList<Checkkoll>();
+		//Mauer
+	protected ArrayList<Checkkoll> Mauer = new ArrayList<Checkkoll>();
 	
 
 	public void init(GameContainer container, StateBasedGame sbg)throws SlickException 
@@ -42,6 +48,8 @@ public class StartGame extends BasicGameState
 		try {
 			InputStream inputStream	= ResourceLoader.getResourceAsStream("res/fonts/Volter__28Goldfish_29.ttf");
 			
+
+			
 			Font awtFont = Font.createFont(Font.TRUETYPE_FONT, inputStream);
 			//Font Größe
 			awtFont = awtFont.deriveFont(24f); 
@@ -50,8 +58,14 @@ public class StartGame extends BasicGameState
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		//Jack aufs Spielfeld setzen
+	    jackobj.add(0, new Jack(320, 320));
+	    //Steuerkeys definieren
+	    ((Jack) jackobj.get(0)).tasteneinstellen(Input.KEY_LEFT, Input.KEY_RIGHT, Input.KEY_UP, Input.KEY_DOWN);
 		
+	    // Map laden
 		mapladen("res/maps/map1.tmx");
+		
 	}
 	
 	public void mapladen(String aktmap) throws SlickException
@@ -72,12 +86,20 @@ public class StartGame extends BasicGameState
 	{
 		container.setVSync(true);
 
+		//Spielfeldgröße definieren
 		float xwert =(float) (float) container.getWidth() / (float) (map.getWidth() * map.getTileWidth()+32); 
 		float ywert =(float) (float) container.getHeight() / (float) (map.getHeight() * map.getTileHeight()); 
 
 		
 		g.scale(xwert, ywert);
+		// Map zeichnen
 		map.render(0,0);
+		
+		//Jack zeichnen
+	    for (Checkkoll ja : jackobj) 
+	    {
+	        ja.draw(g);
+	    }
 		
 		g.setFont(font);
 
@@ -95,17 +117,65 @@ public class StartGame extends BasicGameState
 	public void update(GameContainer container, StateBasedGame sbg, int delta)throws SlickException 
 	{
 		
+		//Jacks Bewegung
+	      for (int i = 0; i < jackobj.size(); i++) 
+	      {
+	          Jack ja = (Jack) jackobj.get(i);
+	          
+	          ja.update(delta, Mauer);
+	          
+	          // Bewegung nach links
+	          if (container.getInput().isKeyDown(ja.blinks())) 
+	          {
+	        	  ja.setPosX(false);
+	        	  if ((ja.getX() % 32) == 0) 
+	        	  {
+	        		  ja.bewegung(-1, 0, Mauer);
+	        	  }
+	          }
+          
+	          // Bewegung nach rechts
+	          if (container.getInput().isKeyDown(ja.brechts())) 
+	          {
+	        	  ja.setPosX(true);
+	        	  if ((ja.getX() % 32) == 0) 
+	        	  {
+	        		  ja.bewegung(1, 0, Mauer);
+	        	  }
+	          }
+	          // Bewegung nach oben
+	          if (container.getInput().isKeyDown(ja.bhoch())) 
+	          {
+	        	  ja.setPosY(false);
+	        	  if ((ja.getY() % 32) == 0) 
+	        	  {
+	        		  ja.bewegung(0, -1, Mauer);
+	        	  }
+	          }
+	          // Bewegung nach unten
+	          if (container.getInput().isKeyDown(ja.brunter())) 
+	          {
+	        	  ja.setPosY(true);
+            
+	        	  if ((ja.getY() % 32) == 0) 
+	        	  {
+	        		  ja.bewegung(0, 1, Mauer);
+	        	  }
+	          }
+	      }
 		
 	}
-	//Bewegen im Menü mit Pfeiltasten
+	
+
 	public void keyReleased(int taste, char c)
 	{
 		
-	// Zurück ins Menü mit Enter oder Leertaste	
+	// Exit aus dem Spiel mit Escape	
 	if(taste == Input.KEY_ESCAPE)
 		{
 			enterStateAndreinit(Menu.stateID);
-
+			//Jack löschen
+		    jackobj.clear();
 			
 		}
 	}

92f71b8888eeba80ed0d2ded5f3a8d24c6b62e19
Milos Ziolkowski <milos.ziolkowski@uni-duesseldorf.de>
2013-07-17 21:55:40 +0200

    pfadfix beim editor

--- src/transevolution/game/Mapeditor.java
+++ src/transevolution/game/Mapeditor.java
@@ -12,69 +12,49 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
-
 @SuppressWarnings("serial")
 public class Mapeditor extends JFrame {
-	 
-    JTextArea textfeld;
- 
-    JButton ladenbutton;
- 
-    JButton speicherbutton;
- 
-    File file = new File("c:/Users/MLZ2/propraworkspace/gruppeB3/res/maps/usermaps/umap1.txt");
- 
-    public Mapeditor() {
-        super("Mapeditor");
- 
-        textfeld = new JTextArea(25, 45);
-        
- /*       ladenbutton = buttonerstellen("Map laden", new ActionListener() 
-        {
-            public void actionPerformed(ActionEvent e) 
-            {
-                try 
-                {
-                	textfeld.read(new FileReader(file), null);
-                } catch (Exception e1) {
-                    e1.printStackTrace();
-                }
-            }
-        });
-   */     
-        try 
-        {
-        	textfeld.read(new FileReader(file), null);
-        } catch (Exception e1) {
-            e1.printStackTrace();
-        }
- 
-        speicherbutton = buttonerstellen("Save", new ActionListener() 
-        {
-            public void actionPerformed(ActionEvent e) 
-            {
-                try 
-                {
-                	textfeld.write(new FileWriter(file));
-                } catch (IOException e2) {
-                    e2.printStackTrace();
-                }
-            }
-        });
-        
-        add(textfeld,BorderLayout.NORTH);
-    //    add(ladenbutton,BorderLayout.CENTER);
-        add(speicherbutton,BorderLayout.SOUTH);
-        
-        pack();
- 
-        setVisible(true);
-    }
- 
-    private JButton buttonerstellen(String caption,
-            ActionListener actionListener) {
-        JButton button = new JButton(caption);
-        button.addActionListener(actionListener);
-        return button;
-    }
+
+	JTextArea textfeld;
+
+	JButton speicherbutton;
+
+	File file = new File(
+			"res/maps/usermaps/umap1.txt");
+
+	public Mapeditor() {
+		super("Mapeditor");
+
+		textfeld = new JTextArea(25, 45);
+
+		try {
+			textfeld.read(new FileReader(file), null);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
+		speicherbutton = buttonerstellen("Save", new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					textfeld.write(new FileWriter(file));
+				} catch (IOException e2) {
+					e2.printStackTrace();
+				}
+			}
+		});
+
+		add(textfeld, BorderLayout.NORTH);
+		add(speicherbutton, BorderLayout.SOUTH);
+
+		pack();
+
+		setVisible(true);
+	}
+
+	private JButton buttonerstellen(String caption,
+			ActionListener actionListener) {
+		JButton button = new JButton(caption);
+		button.addActionListener(actionListener);
+		return button;
+	}
 }
